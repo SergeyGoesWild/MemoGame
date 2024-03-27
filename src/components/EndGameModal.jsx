b20c1d6c859a2d30f36ce558df57e4cafcc8dc92
Sergey Telnov <sergej.telnov@gmail.com>
2024-03-27 13:29:54 +0100

    modals styling done

--- src/components/EndGameModal.jsx
+++ src/components/EndGameModal.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
-import "../styles/InputModal.style.css";
+import "../styles/EndGameModal.style.css";
 
 function EndGameModal({ score, onRestartPress }) {
   return (
     <div>
       <div className="overlay">
         <div className="modal">
-          <div className="form-container">
-            <p>Congrats!</p>
-            <p>Your score is: {score}</p>
-            <button onClick={onRestartPress}>Restart</button>
+          <div className="txt-container">
+            <div className="text">
+              <h4>🥳 Congrats! 🥳</h4>
+              <p>Your score is: {score}</p>
+            </div>
+            <button onClick={onRestartPress} className="button">
+              Restart
+            </button>
           </div>
         </div>
       </div>
--- src/components/InputModal.jsx
+++ src/components/InputModal.jsx
@@ -4,24 +4,32 @@ import "../styles/InputModal.style.css";
 function InputModal({ onSubmitButtonPress }) {
   const [inputValue, setInputValue] = useState("");
   const [inputConform, setInputConform] = useState(false);
+  const [placeHolder, setPlaceHolder] = useState("\u00A0");
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
     let stateVar = Number(event.target.value);
     console.log("Analizing : ", stateVar);
     if (/^\d+$/.test(stateVar)) {
-      if (stateVar < 20 && stateVar > 0) {
+      if (stateVar <= 20 && stateVar > 0) {
         if (stateVar % 2 === 0) {
           console.log("All passed");
           setInputConform(true);
+          setPlaceHolder("\u00A0");
         } else {
           console.log("Division");
+          setInputConform(false);
+          setPlaceHolder("You need an even number");
         }
       } else {
         console.log("Size");
+        setInputConform(false);
+        setPlaceHolder("You need an even number");
       }
     } else {
       console.log("Symbols");
+      setInputConform(false);
+      setPlaceHolder("You need an even number");
     }
   };
 
@@ -36,22 +44,19 @@ function InputModal({ onSubmitButtonPress }) {
     <div>
       <div className="overlay">
         <div className="modal">
-          <form onSubmit={handleSubmit}>
-            <div className="form-container">
-              <label>Enter number of tiles:</label>
-              <input
-                type="text"
-                value={inputValue}
-                onChange={handleInputChange}
-                placeholder="Type here"
-              />
-              {!inputConform && (
-                <span className="no-conform">
-                  You need to enter an even number
-                </span>
-              )}
-              <button type="submit">Submit</button>
-            </div>
+          <form onSubmit={handleSubmit} className="form-container">
+            <label>Enter number of tiles:</label>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Type here"
+              className="input-field"
+            />
+            <span className="no-conform">{placeHolder}</span>
+            <button type="submit" className="button">
+              Submit
+            </button>
           </form>
         </div>
       </div>
